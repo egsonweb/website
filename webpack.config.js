5bd14ea36876690880d78f7f5a355903b0867e4f
Bhargav Anand <rjn143@gmail.com>
2015-12-10 11:41:37 -0800

    chore: add jquery, use bootstrap transition and collapse

--- webpack.config.js
+++ webpack.config.js
@@ -18,13 +18,14 @@ module.exports = {
 
   module: {
     loaders: [
+      { test: /bootstrap-sass\/assets\/javascripts\//, loader: 'imports?jQuery=jquery' },
       { test: /\.js$/, loader: 'react-hot!babel', exclude: /node_modules/, include: path.join(__dirname, 'src')},
       { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
       { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
       { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
       { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' },
-      { test: /\.(png|jpg)$/, loader: 'file-loader?limit=8192' }
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
+      { test: /\.(png|jpg)$/, loader: 'url-loader?limit=25000' }
     ]
   },
 
