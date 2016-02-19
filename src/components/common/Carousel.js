b560220b26506fbdf3d4f6ccd8347f3abdf0c1f0
Bhargav Anand <rjn143@gmail.com>
2016-02-18 19:36:42 -0800

    fix: change fontawesome icon to local fonticon

--- src/components/common/Carousel.js
+++ src/components/common/Carousel.js
@@ -21,7 +21,7 @@ export default class Carousel extends Component {
                   <p>Your search for the right staffing partner ends here.</p>
                   <Link to="contact" className="btn-arrow btn-big btn btn-primary btn-lg hidden-xs">
                     Learn more
-                    <span className="fa fa-arrow-circle-right"></span>
+                    <span className="icon icon-arrow-circle-right"></span>
                   </Link>
                 </div>
               </div>
@@ -35,7 +35,7 @@ export default class Carousel extends Component {
                   <p>Experience the only end to end Contract Hiring process in the staffing industry.</p>
                   <Link to="contact" className="btn-arrow btn-big btn btn-primary btn-lg hidden-xs">
                     Learn more
-                    <span className="fa fa-arrow-circle-right"></span>
+                    <span className="icon icon-arrow-circle-right"></span>
                   </Link>
                 </div>
               </div>
